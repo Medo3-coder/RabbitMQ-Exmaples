b985bd4a445d94dca743946d8b26ed38f8b5a19c
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2023-06-17 21:08:38 +0300

    PubSub implementation

---
+++ PubSub/Publisher/Publisher/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+            factory.Port = 5672;
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            IConnection connection = factory.CreateConnection();
+            IModel channel = connection.CreateModel();
+
+            while (true)
+            {
+                Console.Write("Enter Message:");
+                string message = Console.ReadLine();
+                if(message == "exit") break;
+
+                channel.BasicPublish("ex.fanout" , "" , null , Encoding.UTF8.GetBytes(message));
+
+            }
+
+            channel.Close();
+            connection.Close();
+
+        }
+    }
+}
---
+++ PubSub/Subscriber/Subscriber/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the queue name:");
+            string queueName = Console.ReadLine();
+
+
+            ConnectionFactory factory = new ConnectionFactory();
+            // "guest"/"guest" by default, limited to localhost connections
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+            factory.Port = 5672;
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            IConnection connection = factory.CreateConnection();
+            IModel channel = connection.CreateModel();
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                string message = System.Text.Encoding.UTF8.GetString(body);
+                Console.WriteLine("Subscriber ["+ queueName + "] Message: " + message);
+            };
+
+            var consumerTag = channel.BasicConsume(queueName, true, consumer);
+            Console.WriteLine($"Subscribed to the queue '{queueName}'. Press a key to exit.");
+            Console.ReadKey();
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
