b670c8b0ff092b921b92d46e1aa0ef071078a252
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2023-06-18 13:41:31 +0300

    Request - Reply Coding Demo

---
+++ RequestReply/Replier/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Replier
+{
+    internal class Program
+    {
+
+        //replied application will also be a publisher and subscriber it will subscribe to the request queue and
+        //get request from the requesters process the request and publish the responses to the responses queue
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+            factory.Port = 5672;
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            IConnection connection = factory.CreateConnection();
+            IModel channel = connection.CreateModel();
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                string requests = Encoding.UTF8.GetString(body);
+                Console.WriteLine("request recived: " +  requests);
+
+
+                //response 
+                string response = "response for " + requests;
+                channel.BasicPublish("" , "responses" , null , Encoding.UTF8.GetBytes(response));
+            };
+
+            channel.BasicConsume("requests" , true , consumer);
+            Console.WriteLine("Press a key to exit.");
+            Console.ReadKey();
+
+            connection.Close();
+            channel.Close();
+
+        }
+    }
+}
---
+++ RequestReply/Requestor/Requestor/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Requestor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+            factory.Port = 5672;
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            IConnection connection = factory.CreateConnection();
+            IModel channel = connection.CreateModel();
+
+            //first it will create request and sent or pushlish them to request queue and it will recive response 
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                string message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Response received:" + message);
+
+            };
+
+            //sub to response queue 
+
+            channel.BasicConsume("responses", true , consumer);
+
+             //reading meesage from console and publish it
+
+            while (true)
+            {
+                Console.Write("Enter Your Request:");
+                string request = Console.ReadLine();
+
+                if (request == "exit") break;
+                //publish this request to request queue 
+                channel.BasicPublish("" , "requests" , null, Encoding.UTF8.GetBytes(request));
+            }
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
